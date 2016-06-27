2f80ac5cd8a73f5b76a2408a053acdb3ed9df278
Apple <X@xupengfeidemac-mini.local>
2016-06-27 22:03:26 +0800

    Initial Commit

---
+++ XBanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XBanner
+//
+//  Created by 徐鹏飞 on 16/6/27.
+//  Copyright © 2016年 XBanner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
