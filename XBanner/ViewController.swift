4986a9ace7b320f9667c403a6d9e5f171f5d9501
Apple <X@xupengfeidemac-mini.local>
2016-07-04 23:02:48 +0800

    +++

--- XBanner/ViewController.swift
+++ XBanner/ViewController.swift
@@ -22,28 +22,65 @@ class ViewController: UIViewController {
     }
     
     var timer:XTimer?
+    var timer1:XTimer?
+    var timer2:XTimer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         var i = 20
-        timer = XTimer(time: 1.0) {[weak self]()->Void in
+        
+        timer = XTimer(interval: 1.0, block: { [weak self](o) in
             
             i -= 1
             
-            
-            
             print( i)
+            //print(NSThread.currentThread())
             
             if i == 0
             {
-                self?.timer?.cancel()
+                o.cancel()
                 self?.timer=nil
             }
+
+            
+        })
+        
+        //self.timer?.start()
+        
+        timer1 = XTimer(interval: 1.0, repeats: 10, block: {[weak self] (o) in
+            
+            print("nowTimes: \(o.nowTimes)")
+            
+            if o.nowTimes == 9
+            {
+                self?.timer1 = nil
+            }
+            
+        })
+        
+        //timer1?.start()
+        
+        timer2 = XTimer(delay: 5.0, interval: 1.0, repeats: 1) { [weak self](o) in
+            
+            print("延迟执行!!!!!")
             
         }
         
-        timer?.start()
+        //timer2?.start()
+        
+        XTimer.AsyncDo { 
+            self.timer?.start()
+        }
+        
+        XTimer.AsyncDo {
+            self.timer1?.start()
+        }
+        
+        XTimer.AsyncDo {
+            self.timer2?.start()
+        }
+        
         
         var arr:[XBannerModel] = []
         for i in 1...4
