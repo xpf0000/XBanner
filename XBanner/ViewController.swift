77b0317cea338d90cc3e89cc08cca149300f2386
徐鹏飞 <X@xupengfeidemac-mini.local>
2016-08-07 16:24:37 +0800

    +++

--- XBanner/ViewController.swift
+++ XBanner/ViewController.swift
@@ -17,70 +17,13 @@ class ViewController: UIViewController {
     
     @IBAction func pageClick(sender: UIPageControl) {
         
-        banner.index = sender.currentPage
+        banner.selectIndex = sender.currentPage
         
     }
     
-    var timer:XTimer?
-    var timer1:XTimer?
-    var timer2:XTimer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        var i = 20
-        
-        timer = XTimer(interval: 1.0, block: { [weak self](o) in
-            
-            i -= 1
-            
-            print( i)
-            //print(NSThread.currentThread())
-            
-            if i == 0
-            {
-                o.cancel()
-                self?.timer=nil
-            }
-
-            
-        })
-        
-        //self.timer?.start()
-        
-        timer1 = XTimer(interval: 1.0, repeats: 10, block: {[weak self] (o) in
-            
-            print("nowTimes: \(o.nowTimes)")
-            
-            if o.nowTimes == 9
-            {
-                self?.timer1 = nil
-            }
-            
-        })
-        
-        //timer1?.start()
-        
-        timer2 = XTimer(delay: 5.0, interval: 1.0, repeats: 1) { [weak self](o) in
-            
-            print("延迟执行!!!!!")
-            
-        }
-        
-        //timer2?.start()
-        
-        XTimer.AsyncDo { 
-            self.timer?.start()
-        }
-        
-        XTimer.AsyncDo {
-            self.timer1?.start()
-        }
-        
-        XTimer.AsyncDo {
-            self.timer2?.start()
-        }
-        
         
         var arr:[XBannerModel] = []
         for i in 1...4
--- XBanner/XBanner/XBanner.swift
+++ XBanner/XBanner/XBanner.swift
@@ -15,171 +15,6 @@ class XBannerModel:NSObject
     var obj:AnyObject?
 }
 
-typealias XTimerBlock = (XTimer)->Void
-
-class XTimer:NSObject
-{
-    private var timer:dispatch_source_t!
-    //private var eventBlock:dispatch_block_t!
-    private var eventBlock:XTimerBlock?
-    
-    var delay = 0.0  //延迟时间
-    var interval = 1.0 //每次间隔
-    var leeway:UInt64 = 0 //期望误差范围
-    var repeats = 0
-    
-    private var index = 0
-    
-    var nowTimes:Int{
-        return index
-    }
-    
-    func setEvent(block:XTimerBlock)
-    {
-        eventBlock = block
-    }
-    
-    override init() {
-        super.init()
-    }
-    
-    init(interval:Double) {
-        super.init()
-        self.interval = interval
-    }
-    
-    init(interval:Double,block:XTimerBlock)
-    {
-        super.init()
-        self.interval = interval
-        eventBlock = block
-    }
-    
-    init(interval:Double,leeway:UInt64,block:XTimerBlock)
-    {
-        super.init()
-        self.interval = interval
-        self.leeway = leeway
-        eventBlock = block
-    }
-    
-    init(interval:Double,repeats:Int,block:XTimerBlock)
-    {
-        super.init()
-        self.interval = interval
-        self.repeats = repeats
-        eventBlock = block
-    }
-
-    
-    init(delay:Double,interval:Double,leeway:UInt64,block:XTimerBlock)
-    {
-        super.init()
-        self.interval = interval
-        self.delay = delay
-        self.leeway = leeway
-        eventBlock = block
-    }
-    
-    init(delay:Double,interval:Double,repeats:Int,block:XTimerBlock)
-    {
-        super.init()
-        self.interval = interval
-        self.delay = delay
-        self.repeats = repeats
-        eventBlock = block
-    }
-    
-    init(delay:Double,interval:Double,block:XTimerBlock)
-    {
-        super.init()
-        self.interval = interval
-        self.delay = delay
-        eventBlock = block
-    }
-
-    
-    func start()
-    {
-        self.cancel()
-        
-        // 获得队列
-        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-        
-        // 创建一个定时器(dispatch_source_t本质还是个OC对象)
-        timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue)
-        
-        // 设置定时器的各种属性（几时开始任务，每隔多长时间执行一次）
-        // GCD的时间参数，一般是纳秒（1秒 == 10的9次方纳秒）
-        // 何时开始执行第一个任务
-        // dispatch_time(DISPATCH_TIME_NOW, 1.0 * NSEC_PER_SEC) 比当前时间晚3秒
-        let start = dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC)));
-        let i = UInt64(interval * Double(NSEC_PER_SEC))
-        
-        
-        //其中的dispatch_source_set_timer的最后一个参数，是最后一个参数（leeway），它告诉系统我们需要计时器触发的精准程度。所有的计时器都不会保证100%精准，这个参数用来告诉系统你希望系统保证精准的努力程度。如果你希望一个计时器每5秒触发一次，并且越准越好，那么你传递0为参数。另外，如果是一个周期性任务，比如检查email，那么你会希望每10分钟检查一次，但是不用那么精准。所以你可以传入60，告诉系统60秒的误差是可接受的。他的意义在于降低资源消耗。
-        
-        dispatch_source_set_timer(timer, start, i, leeway)
-        
-        //dispatch_source_set_event_handler(timer, eventBlock)
-        
-        dispatch_source_set_event_handler(timer) {[weak self]()->Void in
-            
-            self?.eventBlock?(self!)
-            
-            self?.index += 1
-            
-            if self?.repeats > 0 && self?.index == self?.repeats
-            {
-                self?.cancel()
-            }
-        }
-
-        dispatch_resume(timer)
-        
-        
-    }
-    
-    func cancel()
-    {
-        if timer != nil
-        {
-            dispatch_source_cancel(timer)
-            timer=nil
-            
-            print("任务完成!!!!!!!!!!!!")
-        }
-        
-    }
-    
-    
-    class func MainDo(block:dispatch_block_t)
-    {
-        dispatch_async(dispatch_get_main_queue(), block)
-    }
-    
-    class func AsyncDo(block:dispatch_block_t)
-    {
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), block)
-    }
-    
-    class func DelayDo(time:NSTimeInterval,block:dispatch_block_t)
-    {
-        let delayInSeconds:Double=time
-        let popTime:dispatch_time_t=dispatch_time(DISPATCH_TIME_NOW, Int64(delayInSeconds * Double(NSEC_PER_SEC)))
-        
-        dispatch_after(popTime, dispatch_get_main_queue(),block)
-        
-    }
-    
-    
-    
-    deinit
-    {
-        print("XTimer deinit !!!!!!!!")
-    }
-}
-
 typealias XBannerClickBlock = (XBannerModel)->Void
 
 typealias XBannerIndexBlock = (Int,XBannerModel)->Void
@@ -187,8 +22,6 @@ typealias XBannerIndexBlock = (Int,XBannerModel)->Void
 class XBanner: UICollectionView ,UICollectionViewDelegate,UICollectionViewDataSource{
 
     let flowLayout = UICollectionViewFlowLayout()
-    
-    
     
     private var clickBlock:XBannerClickBlock?
     private var indexBlock:XBannerIndexBlock?
@@ -219,28 +52,25 @@ class XBanner: UICollectionView ,UICollectionViewDelegate,UICollectionViewDataSo
         }
     }
     
-    
-    
-    var index:Int = 0
+    var selectIndex = 0
     {
         didSet
         {
-            print("index: \(index)")
-            
-            if bannerArr.count > 1
-            {
-                indexBlock?(index,bannerArr[index])
-                scrollToItemAtIndexPath(NSIndexPath.init(forRow: index+2, inSection: 0), atScrollPosition: .Left, animated: true)
-            }
-            
+            if bannerArr.count <= 1 || selectIndex < 0 || selectIndex >= bannerArr.count  {return}
+            index = selectIndex
+            scrollToItemAtIndexPath(NSIndexPath.init(forRow: index+2, inSection: 0), atScrollPosition: .Left, animated: true)
         }
     }
     
-    func pageClick()
+    private var index:Int = 0
     {
-        if bannerArr.count > 1
+        didSet
         {
-            //index = page.currentPage
+            if bannerArr.count > 1
+            {
+                indexBlock?(index,bannerArr[index])
+            }
+            
         }
     }
     
@@ -264,10 +94,6 @@ class XBanner: UICollectionView ,UICollectionViewDelegate,UICollectionViewDataSo
         
         delegate = self
         dataSource = self
-        
-    
-        
-        //page.addTarget(self, action: #selector(pageClick), forControlEvents: .ValueChanged)
         
     }
     
@@ -420,8 +246,6 @@ class XBanner: UICollectionView ,UICollectionViewDelegate,UICollectionViewDataSo
     
     func scrollViewDidScroll(scrollView: UIScrollView) {
         
-        print("offset: \(scrollView.contentOffset.x)")
-        
         if !scrollView.scrollEnabled{return}
 
         if(scrollView.contentOffset.x <= frame.size.width )
@@ -434,15 +258,16 @@ class XBanner: UICollectionView ,UICollectionViewDelegate,UICollectionViewDataSo
             scrollView.contentOffset.x=CGFloat(2)*frame.size.width
         }
         
-        if(Int(scrollView.contentOffset.x*100)%Int(frame.size.width*100)==0)
-        {
-            let nowIndex:Int=Int(Int(scrollView.contentOffset.x*100)/Int(frame.size.width*100))-2;
+            var nowIndex = Int(round(scrollView.contentOffset.x / frame.size.width)) - 2
+    
+            nowIndex = nowIndex < 0 ? bannerArr.count-1 : nowIndex
+            nowIndex = nowIndex >= bannerArr.count ?  0 : nowIndex
+        
             if(nowIndex != index)
             {
                 index = nowIndex
             }
-        }
-        
+
     }
     
 
